ef795ad6a3853be5fd8b36ebc10e938b58ff2179
Ngozi Ekekwe <ngozi-ekekwe>
2021-06-09 18:18:08 +0200

    feat: add get function to get items from dynamo

--- create.js
+++ create.js
@@ -1,20 +1,22 @@
-const AWS = require('aws-sdk');
-const uuid = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
+const AWS = require("aws-sdk");
 
-const client = AWS.DynamoDB.DocumentClient();
-module.exports.run  = async(event) => {
+const client = new AWS.DynamoDB.DocumentClient();
+
+module.exports.run = async event => {
   const data = JSON.parse(event.body);
   const params = {
     TableName: "todos",
-    item: {
-      id: uuid(),
+    Item: {
+      id: uuidv4(),
       text: data.text,
       checked: false
     }
-  }
-  await client.put(params).promise()
+  };
+
+  await client.put(params).promise();
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
-  }
-}
+    body: JSON.stringify(params.Item)
+  };
+};
